37f57615624349a18201525f55bc92d2859c868c
chillzeus <76137066+chillzeus@users.noreply.github.com>
2022-10-28 10:07:39 -0500

    Added player movement, increased canvas size, and gravity

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 canvas = document.getElementById("thisCanvas")
 c = canvas.getContext("2d");
 
-canvas.width = 480;
-canvas.height = 270;
+canvas.width = 1024;
+canvas.height = 576;
 
 c.fillStyle = "black";
 c.fillRect(0, 0, canvas.width, canvas.height);
@@ -12,9 +12,12 @@ let gravity = 0.2;
 const player = {
 	"x": 20,
   "y": 20,
-  "height": 75,
-  "width": 25,
+  "height": 175,
+  "width": 50,
+  "leftpressed": false,
+  "rightpressed": false,
   "velocity_y": 1,
+  "velocity_x": 6,
 }
 
 function draw() {
@@ -24,8 +27,35 @@ function draw() {
 	c.fillRect(player.x, player.y, player.width, player.height)
 }
 
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+  if (e.key == "a") {
+    player.leftpressed = true;
+  }
+  if (e.key == "d") {
+    player.rightpressed = true;
+  }
+}
+function keyUpHandler(e) {
+  if (e.key == "a") {
+    player.leftpressed = false;
+  }
+  if (e.key == "d") {
+    player.rightpressed = false;
+  }
+}
+
+
 function update() {
 	draw();
+  if (player.leftpressed == true) {
+    player.x += player.velocity_x * -1;
+  }
+  if (player.rightpressed == true) {
+    player.x += player.velocity_x;
+  }
   if (player.y < canvas.height - player.height) {
 		player.velocity_y += gravity;  
   }
